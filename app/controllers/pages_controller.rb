542ab3b1d2018d671ebee353a0d9bdabfdcde60e
Tavin Turner <camelinatardis@gmail.com>
2017-06-15 23:53:23 -0600

    Added Coming Soon page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def comingsoon
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  root "posts#index"
+  root "pages#comingsoon"
 
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
