9e13b1f1ea1717fd1fd6f544d2c44171e0046483
Tavin Turner <camelinatardis@gmail.com>
2019-01-04 15:56:37 -0700

    Restyling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  module ApplicationHelper
+    def title(text)
+      content_for :title, text
+    end
+  end
 end
