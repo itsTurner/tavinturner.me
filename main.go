c317c3914e0aec0bc981bd1e9e7b87e96379c9ef
Tavin Turner <camelinatardis@gmail.com>
2019-12-21 23:32:22 -0700

    * tingz

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
   f "fmt"
   h "net/http"
   o "os"
+  i "io/ioutil"
+  s "strings"
 )
+
+type Route struct {
+  URL string
+  File string
+}
+
+func m (i ...interface{}) []interface{} { return i }
 
 func determineListenAddress() (string, error) {
   port := o.Getenv("PORT")
@@ -19,10 +28,25 @@ func main() {
   if err != nil {
     l.Fatal(err)
   }
-  h.HandleFunc("/", func(w h.ResponseWriter, r *h.Request) {
-    h.ServeFile(w, r, "app/index.html")
-  })
-  h.Handle("/css/", h.StripPrefix("/css/", h.FileServer(h.Dir("app/styles/"))))
+
+  csvBytes, _ := i.ReadFile("routes.ssv")
+  csvString := string(csvBytes)
+
+  for _, line := range s.Split(csvString, "\n") {
+    parts := s.Split(line, " ")
+    if len(parts) >= 2 && parts[0][0] != '#' {
+      if parts[0][len(parts[0])-1] == '*' {
+        path := string([]rune(parts[0])[:len(parts[0])-1])
+        h.Handle(path, h.StripPrefix(path, h.FileServer(h.Dir(parts[1]))))
+      } else {
+        route := &Route{ URL: parts[0], File: parts[1] }
+        h.HandleFunc(route.URL, func(w h.ResponseWriter, r *h.Request) {
+          h.ServeFile(w, r, route.File)
+        })
+      }
+    }
+  }
+
   if err := h.ListenAndServe(addr, nil); err != nil {
     panic(err)
   }
